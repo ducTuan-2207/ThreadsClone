b405c2eb13589fb7ad1a1322a4023e4c1b8c3aaa
DucTuan <tuanhypc02@gmail.com>
2024-04-15 22:59:23 +0700

    Initial Commit

---
+++ ThreadsClone/ThreadsCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThreadsCloneApp.swift
+//  ThreadsClone
+//
+//  Created by macOS on 15/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct ThreadsCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
