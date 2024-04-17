65d10f5ac300f11ae872cdee95692b24f253d977
DucTuan <tuanhypc02@gmail.com>
2024-04-17 16:41:09 +0700

    explore view

--- ThreadsClone/Core/Explore/View/ExploreView.swift
+++ ThreadsClone/Core/Explore/View/ExploreView.swift
@@ -8,8 +8,44 @@
 import SwiftUI
 
 struct ExploreView: View {
+    @State private var searchText = ""
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            ScrollView {
+                LazyVStack {
+                    ForEach(0 ... 10, id: \.self){user in
+                        VStack {
+                            HStack {
+                                Image("rose")
+                                    .resizable()
+                                    .frame(width: 40,height: 40)
+                                    .clipShape(Circle())
+                                VStack(alignment: .leading){
+                                    Text("Rose")
+                                        .fontWeight(.semibold)
+                                    Text("Blackpink")
+                                }
+                                .font(.footnote)
+                                Spacer()
+                                Text("Follow")
+                                    .font(.subheadline)
+                                    .fontWeight(.semibold)
+                                    .frame(width: 100, height: 32)
+                                    .overlay {
+                                        RoundedRectangle(cornerRadius: 30)
+                                            .stroke(Color(.systemGray4), lineWidth: 1)
+                                    }
+                            }
+                            .padding(.horizontal)
+                        }
+                        Divider()
+                    }
+                    .padding(.vertical, 4)
+                }
+            }
+            .navigationTitle("Search")
+            .searchable(text: $searchText, prompt: "Search")
+        }
     }
 }
 
