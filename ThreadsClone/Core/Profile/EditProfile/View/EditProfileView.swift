f2bbbaec0b15c6b9a735aa4fd7d03b641134d52f
DucTuan <tuanhypc02@gmail.com>
2024-04-20 00:59:48 +0700

    done edit profile view

--- ThreadsClone/Core/Profile/EditProfile/View/EditProfileView.swift
+++ ThreadsClone/Core/Profile/EditProfile/View/EditProfileView.swift
@@ -8,14 +8,53 @@
 import SwiftUI
 
 struct EditProfileView: View {
+    @State private var bioText = ""
+    @State private var linkText = ""
+    @State private var isPrivatePofile = false
     var body: some View {
         NavigationStack {
             ZStack {
                 Color(.systemGroupedBackground)
                     .edgesIgnoringSafeArea([.bottom,.horizontal])
                 VStack {
-                    
+                    HStack {
+                        VStack(alignment: .leading) {
+                            Text("Name")
+                                .fontWeight(.semibold)
+                            Text("Duc Tuan Chanel")
+                            
+                        }
+                        .font(.footnote)
+                        Spacer()
+                        CirculaProfileImageView()
+                    }
+                    Divider()
+                    //bio field
+                    VStack(alignment: .leading) {
+                        Text("Bio")
+                            .fontWeight(.semibold)
+                        TextField("Enter your bio ...", text: $bioText, axis: .vertical)
+                    }
+                    Divider()
+                    //links
+                    VStack(alignment: .leading) {
+                        Text("Links")
+                            .fontWeight(.semibold)
+                        TextField("Enter your links ...", text: $linkText, axis: .vertical)
+                    }
+                    Divider()
+                    // private profile
+                    Toggle("Private profile", isOn: $isPrivatePofile)
                 }
+                .font(.footnote)
+                .padding()
+                .background(.white)
+                .cornerRadius(10)
+                .overlay {
+                    RoundedRectangle(cornerRadius: 10)
+                        .stroke(Color(.systemGray4), lineWidth: 1)
+                }
+                .padding()
             }
             .navigationTitle("Edit Profile")
             .navigationBarTitleDisplayMode(.inline)
